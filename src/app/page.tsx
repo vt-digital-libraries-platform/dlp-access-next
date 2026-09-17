f12fc9e5033a69cdc70950519f9bf671fa14f722
Kelly Ervin Mills <kellyme@vt.edu>
2026-09-17 15:23:44 -0400

    Remove extra <main> from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-      <main>
+      <>
         <h1>Welcome to the Home Page</h1>
         <p>This is the home page of the application.</p>
-      </main>
+      </>
   );
 }
