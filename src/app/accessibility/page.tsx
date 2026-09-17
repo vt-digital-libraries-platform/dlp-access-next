8219fdd6f9a06dc71f875323cd8d7275e880858f
Kelly Ervin Mills <kellyme@vt.edu>
2026-09-17 15:25:20 -0400

    Add static pages routes

---
+++ src/app/accessibility/page.tsx
@@ -0,0 +1,29 @@
+export default function AboutPage() {
+  return (
+    <>
+      <h1>Accessibility</h1>
+      <p className="statement">This Digital Library is a public resource provided by the University Libraries at Virginia Tech for research and preservation purposes. Virginia Tech is committed to ensuring equal access and strives to maintain a barrier-free environment.</p>
+      <p>For more information on accessibility including University policies, relevant legislation, and support resources go to <a href="https://accessibility.vt.edu/" target="_blank" rel="noopener noreferrer">Virginia Tech's Accessibility Portal</a>.</p>
+
+      <h2>Report an Issue</h2>
+      <p><span>If you've encountered an accessibility issue with any part of this platform or its contents, please </span><a href="mailto:digitallibraries@vt.edu">Report an Accessibility Issue</a>.</p>
+      
+      <h2>Our Strategy</h2>
+      <p><em>We are finalizing our formal accessibility statement and continuing accessibility strategy. This page will be updated with that information when it is available.</em></p>
+
+      <h3>Web Platform Accessibility</h3>
+      <p>Throughout our software lifecycle, we use a combination of automated testing methods, including continuous site monitoring and targeted page testing, and manual testing methods, including with keyboard navigation, screen readers, and modified user settings like content magnigication and reduced motion. As we develop and deploy updates, we are continually working towards accessibility in conformance with <a href="https://www.w3.org/TR/WCAG21/" target="_blank" rel="noopener noreferrer">the WCAG 2.1 standard</a> at the AA level, or higher.</p>
+
+      <h3>Historical Content Accessibility</h3>
+      <p>This platform houses a wide range of collections of physical historical materials which have been manually digitized and don't inherently conform to modern accessibility standards. We are conducting research on text detection and extraction from historical handwritten letters, maps, and more and using the knowledge gained to develop new processes for providing the contents of these digitized historical collections in more accessible formats.</p>
+
+      <h4>Archived Content</h4>
+      <p>In conformance with the <a href="https://www.ada.gov/law-and-regs/regulations/title-ii-2010-regulations/" target="_blank" rel="noopener noreferrer">ADA Title II Regulations</a> for web content, we have "Archived" our historical content. All content is still visible on the platform and available for research and reference purposes but may not yet have a fully accessible format, like complete and accurate extracted text for a handwritten letter.</p>
+      <p>If you are an individual with a disability and require an accessible version of any archived content, please <a href="mailto:digitallibraries@vt.edu">contact us</a> and we will glady provide a remediated version to you.</p>
+      <p>To learn more about the Archived Web Content Exception, please refer to the <a href="https://www.ada.gov/resources/2024-03-08-web-rule/#summary-of-the-exceptions" target="_blank" rel="noopener noreferrer">ADA Title II Fact Sheet's Summary of Exceptions</a>.</p>
+      
+      <h2>Virginia Tech's Accessibility Statement</h2>
+      <p>Individuals with disabilities contribute to our campus's cultural and intellectual diversity. Virginia Polytechnic Institute and State University is committed to non-discrimination in employment and education in compliance with the Americans with Disabilities Amendments Act of 2008, Sections 504 and 508 of the Rehabilitation Act of 1973, as amended, as well as Virginia's IT Access Act.</p>
+    </>
+  )
+}
